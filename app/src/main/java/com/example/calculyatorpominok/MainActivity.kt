33b821658e5a73fdde56e7e033355abaee11d313
Olla <prohoda_o@mail.ru>
2023-01-31 16:26:06 +0300

    + 3, 40 Days

--- app/src/main/java/com/example/calculyatorpominok/MainActivity.kt
+++ app/src/main/java/com/example/calculyatorpominok/MainActivity.kt
@@ -12,7 +12,9 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private var textViewDateOfDeath: TextView? = null
+    private var textViewDateOfDeathThree: TextView? = null
     private var textViewDateOfDeathNine: TextView? = null
+    private var textViewDateOfDeathForty: TextView? = null
     private var button: Button? = null
     private var datePicker: MaterialDatePicker<Long>? = null
 
@@ -21,7 +23,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textViewDateOfDeath = findViewById(R.id.textDateOfDeath)
+        textViewDateOfDeathThree = findViewById(R.id.textDateOfDeathThree)
         textViewDateOfDeathNine = findViewById(R.id.textDateOfDeathNine)
+        textViewDateOfDeathForty = findViewById(R.id.textDateOfDeathForty)
         button = findViewById(R.id.button)
         datePicker = MaterialDatePicker.Builder.datePicker()
             .setTitleText("В")
@@ -29,10 +33,16 @@ class MainActivity : AppCompatActivity() {
         datePicker?.addOnPositiveButtonClickListener { selection ->
             val dateOfDeath = getDate(selection, DATE_FORMAT)
             textViewDateOfDeath?.text = dateOfDeath
+            val threeDay = getDate(selection + THREE_DATE, DATE_FORMAT)
+            textViewDateOfDeathThree?.text = threeDay
+            textViewDateOfDeathThree?.visibility = View.VISIBLE
             val nineDate = getDate(selection + NINE_DATE, DATE_FORMAT)
             textViewDateOfDeathNine?.text = nineDate
             textViewDateOfDeathNine?.visibility = View.VISIBLE
             Log.d ("ninedate = ", nineDate)
+            val fortyDate = getDate(selection + FORTY_DATE, DATE_FORMAT)
+            textViewDateOfDeathForty?.text = fortyDate
+            textViewDateOfDeathForty?.visibility = View.VISIBLE
         }
         button?.setOnClickListener { datePicker?.show(supportFragmentManager, "tag") }
     }
@@ -40,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         private const val DATE_FORMAT = "dd.MM.yyyy"
         private const val THREE_DATE = 1000 * 60 * 60 * 24 * (3-1)
         private const val NINE_DATE = 1000 * 60 * 60 * 24 * (9-1)
-        private const val FORTY_DATE = 1000 * 60 * 60 * 24 * (40-1)
+        private const val FORTY_DATE: Long = 1000L * 60L * 60L * 24L * (40L-1L)
     }
 }
 
@@ -49,5 +59,5 @@ fun getDate(milliSeconds: Long, dateFormat: String?): String {
 
     val calendar: Calendar = Calendar.getInstance()
     calendar.timeInMillis = milliSeconds
-    return formatter.format(calendar.getTime())
+    return formatter.format(calendar.time)
 }
