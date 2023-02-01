0cdd5a922cc8958cfb4c1dc7d44c6ea2c2b85d58
Olla <prohoda_o@mail.ru>
2023-02-01 20:58:12 +0300

    constraint to all of date, + six month, one year

--- app/src/main/java/com/example/calculyatorpominok/MainActivity.kt
+++ app/src/main/java/com/example/calculyatorpominok/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     private var textViewDateOfDeathThree: TextView? = null
     private var textViewDateOfDeathNine: TextView? = null
     private var textViewDateOfDeathForty: TextView? = null
+    private var textViewDateOfDeathSixMonth: TextView? = null
+    private var textViewDateOfDeathOneYear: TextView? = null
     private var button: Button? = null
     private var datePicker: MaterialDatePicker<Long>? = null
 
@@ -26,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         textViewDateOfDeathThree = findViewById(R.id.textDateOfDeathThree)
         textViewDateOfDeathNine = findViewById(R.id.textDateOfDeathNine)
         textViewDateOfDeathForty = findViewById(R.id.textDateOfDeathForty)
+        textViewDateOfDeathSixMonth = findViewById(R.id.textDateOfDeathSixMonths)
+        textViewDateOfDeathOneYear = findViewById(R.id.textDateOfDeathOneYear)
         button = findViewById(R.id.button)
         datePicker = MaterialDatePicker.Builder.datePicker()
             .setTitleText("В")
@@ -43,6 +47,12 @@ class MainActivity : AppCompatActivity() {
             val fortyDate = getDate(selection + FORTY_DATE, DATE_FORMAT)
             textViewDateOfDeathForty?.text = fortyDate
             textViewDateOfDeathForty?.visibility = View.VISIBLE
+            val sixMonthDate = getDate(selection + SIXMONTH_DATE, DATE_FORMAT)
+            textViewDateOfDeathSixMonth?.text = sixMonthDate
+            textViewDateOfDeathSixMonth?.visibility = View.VISIBLE
+            val oneYearDate = getDate(selection + ONEYEAR_DATE, DATE_FORMAT)
+            textViewDateOfDeathOneYear?.text = oneYearDate
+            textViewDateOfDeathOneYear?.visibility = View.VISIBLE
         }
         button?.setOnClickListener { datePicker?.show(supportFragmentManager, "tag") }
     }
@@ -51,6 +61,9 @@ class MainActivity : AppCompatActivity() {
         private const val THREE_DATE = 1000 * 60 * 60 * 24 * (3-1)
         private const val NINE_DATE = 1000 * 60 * 60 * 24 * (9-1)
         private const val FORTY_DATE: Long = 1000L * 60L * 60L * 24L * (40L-1L)
+        private const val SIXMONTH_DATE: Long = 1000L * 60L * 60L * 24L * (180L-1L)
+        private const val ONEYEAR_DATE: Long = 1000L * 60L * 60L * 24L * (365L)
+
     }
 }
 
