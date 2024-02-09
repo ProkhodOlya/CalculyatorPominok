79f30ee78c74bb7cd034720c9cdd5582101fba5d
Olla <prohoda_o@mail.ru>
2024-02-09 15:52:13 +0300

    change color of back arrow

--- app/src/main/java/com/calculyatorpominok/presentation/details/DetailsFragment.kt
+++ app/src/main/java/com/calculyatorpominok/presentation/details/DetailsFragment.kt
@@ -1,11 +1,14 @@
 package com.calculyatorpominok.presentation.details
 
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.text.HtmlCompat
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY
 import androidx.fragment.app.Fragment
@@ -13,8 +16,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.calculyatorpominok.utils.ARGS_DAY_OF_COMMEMORATION
 import com.calculyatorpominok.R
+import com.calculyatorpominok.utils.ARGS_DAY_OF_COMMEMORATION
 import kotlinx.coroutines.launch
 
 class DetailsFragment : Fragment() {
@@ -31,7 +34,19 @@ class DetailsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_details, container, false)
 
         toolbar = view.findViewById<Toolbar?>(R.id.toolbar).apply {
-            setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
+            val backIcon = AppCompatResources.getDrawable(
+                context,
+                androidx.appcompat.R.drawable.abc_ic_ab_back_material
+            )
+            val typedValue = TypedValue();
+            requireContext().theme.resolveAttribute(
+                androidx.appcompat.R.attr.colorPrimary,
+                typedValue,
+                true
+            );
+            val color = ContextCompat.getColor(context, typedValue.resourceId)
+            backIcon?.setTint(color)
+            navigationIcon = backIcon
             setNavigationOnClickListener {
                 parentFragmentManager.popBackStack()
             }
